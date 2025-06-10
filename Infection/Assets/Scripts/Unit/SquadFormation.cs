fb726a92d3c6e658e18ed9f15436b69fcb1bdc61
ayumuOhara <kmm2459310@stu.o-hara.ac.jp>
2025-06-10 11:51:29 +0900

    【ユニット・機能追加】　編成機能を実装
    ■編成
    ・ボタンを押すことでリーダーを選ぶことができる。
    ・スライダーのつまみを左右に動かすことで、部隊の人数の増減を行うことができる。
    ・リーダーを切り替えると、人数によるステータス補正、ステータス表示が反映される。

--- Infection/Assets/Scripts/Unit/SquadFormation.cs
+++ Infection/Assets/Scripts/Unit/SquadFormation.cs
@@ -1,44 +1,118 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 // 部隊のステータス管理
-[SerializeField]
 public class SquadStats
 {
-    UnitStats leaderUnit;   // リーダーユニットのパラメータ
+    public UnitStats leaderUnit { get; private set; }   // リーダーユニットのパラメータ
 
-    int squadMemberCnt;     // 部隊のメンバー数
+    float defaultHp = 0;
+    float defaultAtk = 0;
+    float defaultVirusPow = 0;
+    float defaultSpd = 0;
+
+    public int squadMemberCnt;
+    int squadMemberMinCnt = 0;
+    int squadMemberMaxCnt = 100;
     bool isDead;            // 部隊が壊滅したか
+
+    // 雑兵のメンバー数をセット
+    public void SetSoldierCnt(int value)
+    {
+        squadMemberCnt = value;
+    }
 
     // リーダーのパラメータをセット
     public void SetLeaderStats(UnitStats leader)
     {
         leaderUnit = leader;
+
+        defaultHp = leader.hp;
+        defaultAtk = leader.atk;
+        defaultVirusPow = leader.virusPow;
+        defaultSpd = leader.spd;
     }
 
     // 部隊のパラメータをセット
-    public void SetSquadStats(int member)
+    public void SetSquadStats()
     {
-        float correction = member * (1 / 100);      // 部隊の人数 * 1%の補正値
+        Debug.Log("パラメータをセット");
 
-        leaderUnit.hp       += leaderUnit.hp       * correction;
-        leaderUnit.atk      += leaderUnit.atk      * correction;
-        leaderUnit.virusPow += leaderUnit.virusPow * correction;
-        leaderUnit.spd      -= leaderUnit.spd      * correction;
+        float correction = squadMemberCnt * 0.01f;      // 部隊の人数 * 1%の補正値
+
+        leaderUnit.hp       = defaultHp       + defaultHp       * correction;
+        leaderUnit.atk      = defaultAtk      + defaultAtk      * correction;
+        leaderUnit.virusPow = defaultVirusPow + defaultVirusPow * correction;
+        float slowRate = (float)squadMemberCnt / squadMemberMaxCnt;
+        leaderUnit.spd = defaultSpd * (1 - slowRate);
+
+        //Debug.Log(leaderUnit.hp);
+        //Debug.Log(leaderUnit.atk);
+        //Debug.Log(leaderUnit.virusPow);
+        //Debug.Log(leaderUnit.spd);
     }
 }
 
 // 部隊の編成管理
 public class SquadFormation : MonoBehaviour
 {
+    [SerializeField] UnitStatsData unitStatsData;
+
+    public SquadStats squadStats { get; private set; }
+
+    [SerializeField] Slider soldierSlider;
+    int squadMemberCnt;
+
+    // UnitStats を new で複製する関数を作る
+    public UnitStats Clone(UnitStats original)
+    {
+        return new UnitStats
+        {
+            unitCode = original.unitCode,
+            unitName = original.unitName,
+            leaderSkill = original.leaderSkill,
+            role = original.role,
+            lv = original.lv,
+            hp = original.hp,
+            atk = original.atk,
+            virusPow = original.virusPow,
+            atkSpd = original.atkSpd,
+            spd = original.spd,
+            isFly = original.isFly,
+            range = original.range,
+            cost = original.cost,
+            sortieCoolTime = original.sortieCoolTime
+        };
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        squadStats = new SquadStats();
+        Debug.Log(squadStats);
+
+        squadStats.SetLeaderStats(Clone(unitStatsData.UnitParameter[0]));
+
+        int squadMemberMinCnt = 0;      // デバッグ用
+        int squadMemberMaxCnt = 100;   // デバッグ用
+
+        // スライダーの最小、最大値設定
+        soldierSlider.minValue = squadMemberMinCnt;
+        soldierSlider.maxValue = squadMemberMaxCnt;
+
+        soldierSlider.onValueChanged.AddListener(OnSliderSoldier);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickLeader(int num)
     {
-        
+        squadStats.SetLeaderStats(Clone(unitStatsData.UnitParameter[num]));
+        squadStats.SetSquadStats();
+    }
+
+    void OnSliderSoldier(float value)
+    {
+        squadStats.SetSoldierCnt((int)value);
+        squadStats.SetSquadStats();
     }
 }
