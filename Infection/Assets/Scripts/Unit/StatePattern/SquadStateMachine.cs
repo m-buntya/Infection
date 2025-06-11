0c3c3827f6eb121a5279acb8110b2b50a12e85c4
ayumuOhara <kmm2459310@stu.o-hara.ac.jp>
2025-06-11 11:37:20 +0900

    【ユニット】　部隊をフィールド上に出した時の挙動を作成

--- Infection/Assets/Scripts/Unit/StatePattern/SquadStateMachine.cs
+++ Infection/Assets/Scripts/Unit/StatePattern/SquadStateMachine.cs
@@ -1,6 +1,47 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class SquadStateMachine
+namespace StatePatteren.State
 {
-    
+    [SerializeField]
+    public class SquadStateMachine
+    {
+        public SquadState CurrenState { get; private set; }
+
+        public MoveState moveState;
+        public AttackState attackState;
+        public DeadState deadState;
+        
+        public SquadStateMachine(SquadController squadController)
+        {
+           this.moveState = new MoveState(squadController);
+           this.attackState = new AttackState(squadController);
+           this.deadState = new DeadState(squadController);
+        }
+
+        public void Initialize(SquadState state)
+        {
+            CurrenState = state;
+            state.Enter();
+        }
+
+        public void TransitionTo(SquadState nextState)
+        {
+            Debug.Log($"{CurrenState}Ç©ÇÁ{nextState}Ç÷à⁄çsÇµÇ‹Ç∑");
+
+            CurrenState.Exit();
+            CurrenState = nextState;
+            nextState.Enter();
+        }
+
+        public void Update()
+        {
+            CurrenState?.Update();
+        }
+
+        public void Transition()
+        {
+            CurrenState?.Transition();
+        }
+    }
 }
