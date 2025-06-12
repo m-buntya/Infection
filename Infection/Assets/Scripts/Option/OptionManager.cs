abd07b0df568eef4b334186379c80d84f297c72d
kmm-2459316 <kmm2459316@stu.o-hara.ac.jp>
2025-06-12 12:41:23 +0900

    【機能実装】OptionManagerをOptionButtonにつける。
    OptionManagerをOptionButtonにつけて、シーンによって表示するしない、移動するしないの機能を実装

--- Infection/Assets/Scripts/Option/OptionManager.cs
+++ Infection/Assets/Scripts/Option/OptionManager.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OptionManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //しょっぱな起動
+    void Awake()
     {
-        
+        DontDestroyOnLoad(transform.root.gameObject);
+        SceneManager.sceneLoaded += SceneCheck;
     }
 
-    // Update is called once per frame
-    void Update()
+    //規定シーン以外では非表示
+    public void SceneCheck(Scene scene, LoadSceneMode mode)
     {
-        
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "TitleScene" || currentScene.name == "HomeScene")
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+        Positioning(scene);
+    }
+
+    //シーン別のポジショニング
+    public void Positioning(Scene scene)
+    {
+        if (scene.name == "TitleScene")
+        {
+            RectTransform rt = GetComponent<RectTransform>();
+            rt.anchoredPosition = new Vector2(800, -430);
+            rt.sizeDelta = new Vector2(300, 200);
+        }
+        else if (scene.name == "HomeScene")
+        {
+            RectTransform rt = GetComponent<RectTransform>();
+            rt.anchoredPosition = new Vector2(650, 460);
+            rt.sizeDelta = new Vector2(200, 140);
+        }
     }
 }
