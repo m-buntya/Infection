e485a7282bc5b8a59fb2b0f0c83fcacac2bdeb4a
kmm-2459308 <kmm2459308@stu.o-hara.ac.jp>
2025-06-27 14:57:35 +0900

    【機能実装】自分のシーンでユニットの挙動を確認できるようにしました
    自分のシーンに竹下君のシーンのHierarchyをコピペして自分のシーンで確認できるようにした。
    アーチャーユニットが攻撃できるようにArcherBehavior.csのスクリプトを書き換えました。

--- Infection/Assets/Scripts/Unit/StrategyPattern/ArcherBehavior.cs
+++ Infection/Assets/Scripts/Unit/StrategyPattern/ArcherBehavior.cs
@@ -5,15 +5,17 @@ namespace StrategyPatteren.Role
 {
     public class ArcherBehavior : IRoleBehavior
     {
+        UnitManager unitManager;
+
         public void Action(UnitController unit)
         {
             GetTargetSystem getTarget = new GetTargetSystem();
-            //var target = getTarget.GetTarget("UnitEnemy", unit.gameObject)?.GetComponent<EnemyController>();     // UŒ‚‘ÎÛ‚Ìæ“¾
-            //if (target != null)
-            //{
-            //    Debug.Log($"ArcherFUŒ‚‘ÎÛF{target}");
-            //    target.TakeDamage(unit.unitStats.atk);     // UŒ‚—Í•ªƒ_ƒ[ƒW‚ğ—^‚¦‚é
-            //}
+            var target = getTarget.GetTarget(unit.gameObject, "Enemy")?.GetComponent<UnitController>();     // UŒ‚‘ÎÛ‚Ìæ“¾
+            if (target != null)
+            {
+                Debug.Log($"ArcherFUŒ‚‘ÎÛF{target}");
+                target.TakeDamage(unit.unitStats.atk);     // UŒ‚—Í•ªƒ_ƒ[ƒW‚ğ—^‚¦‚é
+            }
         }
     }
 }
