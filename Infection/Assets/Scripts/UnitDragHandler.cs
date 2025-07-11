b429894728fc8237cf9b6ceb31af78df84fd3cee
kmm-2459308 <kmm2459308@stu.o-hara.ac.jp>
2025-07-11 10:28:09 +0900

    【機能変更】ドラッグのユニット表示をImageからObjectで置けるように変更

--- Infection/Assets/Scripts/UnitDragHandler.cs
+++ Infection/Assets/Scripts/UnitDragHandler.cs
@@ -5,10 +5,8 @@ using UnityEngine.UI;
 public class UnitDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public GameObject unitPrefab;
-    public Image dragIconPrefab;
 
-    private Image currentIcon;
-    private RectTransform iconRect;
+    private GameObject dragPreviewObject;
     private Camera cam;
 
     private void Start()
@@ -18,22 +16,27 @@ public class UnitDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        currentIcon = Instantiate(dragIconPrefab, transform.root);
-        iconRect = currentIcon.GetComponent<RectTransform>();
-        currentIcon.raycastTarget = false;
+        // ドラッグ中に見せる仮オブジェクトを生成
+        dragPreviewObject = Instantiate(unitPrefab);
+        SetDragPreviewAlpha(dragPreviewObject, 0.5f); // 半透明に
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (iconRect != null)
+        if (dragPreviewObject != null)
         {
-            iconRect.position = eventData.position;
+            Vector3 worldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            worldPos.z = 0f;
+            dragPreviewObject.transform.position = worldPos;
         }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (currentIcon != null) Destroy(currentIcon.gameObject);
+        if (dragPreviewObject != null)
+        {
+            Destroy(dragPreviewObject); // 仮オブジェクトは削除
+        }
 
         Vector3 worldPos = cam.ScreenToWorldPoint(Input.mousePosition);
         worldPos.z = 0f;
@@ -41,7 +44,19 @@ public class UnitDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
         if (hit.collider != null && hit.collider.CompareTag("DropField"))
         {
+            // 本物を配置
             Instantiate(unitPrefab, hit.collider.transform.position, Quaternion.identity);
+        }
+    }
+
+    /// 半透明表示用：SpriteRendererの色を調整
+    private void SetDragPreviewAlpha(GameObject obj, float alpha)
+    {
+        foreach (var sr in obj.GetComponentsInChildren<SpriteRenderer>())
+        {
+            Color c = sr.color;
+            c.a = alpha;
+            sr.color = c;
         }
     }
 }
