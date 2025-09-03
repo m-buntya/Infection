c904462a394156d797c26186d3aff262ebded383
2459338 <kmm2459338@stu.o-hara.ac.jp>
2025-09-03 13:42:54 +0900

    HP増減

---
+++ Infection/Assets/Scripts/Unit/UnitAttackBace.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class UnitAttackBace:MonoBehaviour
+{
+    public UnitStats unitStats; //ユニットのステータス
+    public bool isEnemyUnit = false; //このユニットが敵かどうか
+    public float attackInterval= 1f;
+    private float attackTimer = 0f;
+
+    private void Update()
+    {
+        attackTimer += Time.deltaTime;
+    }
+
+    void OnTrrigerStay2D(Collider2D other)
+    {
+        if (attackTimer >= attackInterval)
+        {
+                GameClearController gameClearController = FindObjectOfType<GameClearController>();
+            if (gameClearController != null) return;
+            int damage = Mathf.RoundToInt(unitStats.atk);
+            if (isEnemyUnit && other.CompareTag("PlayerBace"))
+            {
+                gameClearController.ApplyDamageToBase(false, damage); //プレイヤー拠点にダメージ
+                Debug.Log($"{unitStats.unitName}（敵）がプレイヤー拠点に{damage}ダメージを与えた");
+                attackTimer = 0f;
+            }
+            else if (!isEnemyUnit && other.CompareTag("EnemyBace"))　//敵拠点にダメージ
+            {
+                gameClearController.ApplyDamageToBase(true, damage);
+                Debug.Log($"{unitStats.unitName}（味方）が敵拠点に{damage}ダメージを与えた");
+                attackTimer = 0f;
+            }
+            }
+        
+    }
+}
