5fe36a8360710c7f48a85a4b0c0c37b87842a247
ayumuOhara <kmm2459310@stu.o-hara.ac.jp>
2025-10-01 14:56:57 +0900

    【バグ修正】　シーン遷移時に発生していたエラーを解消

--- Infection/Assets/Scripts/UnitDragHandler.cs
+++ Infection/Assets/Scripts/UnitDragHandler.cs
@@ -13,7 +13,7 @@ public static class WaitEndDrag
 
 public class UnitDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    CostManager costManager;
+    [SerializeField] CostManager costManager;
     UnitGenerater unitGenerator;
 
     [Header("配置するユニットプレハブ")]
@@ -41,7 +41,6 @@ public class UnitDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     {
         cam = Camera.main;
         isDrag = false;
-        costManager = GameObject.Find("GameManager").GetComponent<CostManager>();
         unitGenerator = GameObject.Find("UnitGenerater").GetComponent<UnitGenerater>(); 
     }
 
@@ -53,7 +52,7 @@ public class UnitDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     // コストが足りているか
     public void CostCheck()
     {
-        if(unitGenerator.GetStats(gameObject).cost <= 0)
+        if (unitGenerator.GetStats(gameObject).cost <= 0)
         {
             isDrag = false;
         }
